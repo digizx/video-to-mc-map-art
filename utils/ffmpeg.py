38a5a8c78aa2f58d585ced0c17f60b392e50c79d
migurd <angelquimorita@gmail.com>
2025-05-04 19:55:57 -0700

    refactor: changed ffmpeg util variables path style to have it all in a single variable

--- utils/ffmpeg.py
+++ utils/ffmpeg.py
@@ -8,10 +8,8 @@ INPUT_DIR = os.path.join(ROOT_DIR, 'src')
 OUTPUT_DIR = os.path.join(ROOT_DIR, 'out')
 
 def transform_video(
-        input_path: str = None,
-        input_filename: str = None,
-        output_path: str = None,
-        output_filename: str = None,
+        path_input: str = None,
+        path_output: str = None,
         out_size: str = '128',
         out_fps: int = 40
     ):
@@ -20,16 +18,13 @@ def transform_video(
     a new video using FFMPEG
     """
 
-    input_file = os.path.join(input_path, input_filename)
-    output_file = os.path.join(output_path, output_filename)
-
     command = [
         'ffmpeg',
         '-y',  # overwrite output
-        '-i', input_file,
+        '-i', path_input,
         '-vf', 'crop=\'min(in_w\\,in_h)\':\'min(in_w\\,in_h)\',scale={0}:{0}:flags=spline'.format(out_size), # spline is the best looking one
         '-r', str(out_fps),
-        output_file,
+        path_output,
         '-hide_banner',
         '-loglevel', 'error'
     ]
@@ -37,21 +32,19 @@ def transform_video(
     subprocess.run(command, check=True)
 
 def video_to_frames(
-    input_path: str = None,
-    input_filename: str = None
+    path_input: str = None,
     ):
     """
     Function that receives a video and outputs all it's frames
     """
-    input_file = os.path.join(input_path, input_filename)
     output_pattern = os.path.join(OUTPUT_DIR, 'frames', 'out%d.png')
 
-    frames_dir = os.path.join(input_path, 'frames')
+    frames_dir = os.path.join(OUTPUT_DIR, 'frames')
     os.makedirs(frames_dir, exist_ok=True)
 
     command = [
         'ffmpeg',
-        '-i', input_file,
+        '-i', path_input,
         output_pattern,
         '-hide_banner',
         '-loglevel', 'error'
@@ -73,14 +66,16 @@ if (__name__ == "__main__"):
 
     clean_up_out()
 
+    path_input_tr = os.path.join(INPUT_DIR, 'odore_china_test.mp4')
+    path_output_tr = os.path.join(OUTPUT_DIR, 'temp.mp4')
+
     transform_video(
-        input_path=INPUT_DIR,
-        input_filename='odore_china_test.mp4',
-        output_path=OUTPUT_DIR,
-        output_filename='temp.mp4',
+        path_input=path_input_tr,
+        path_output=path_output_tr
     )
 
+    path_input_vf = os.path.join(OUTPUT_DIR, 'temp.mp4')
+
     video_to_frames(
-        input_path=OUTPUT_DIR,
-        input_filename='temp.mp4'
+        path_input=path_input_vf
     )
