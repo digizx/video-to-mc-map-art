1c504a67535266b77ce83ff2dfc02e42a0520723
migurd <angelquimorita@gmail.com>
2025-05-25 01:37:20 -0700

    feature: ahora se puede agregar un video dentro del juego con el tope de 4.551 horas

--- utils/DatapackGeneratorUtils.py
+++ utils/DatapackGeneratorUtils.py
@@ -1,7 +1,9 @@
 import io
 import os
+import pprint
 import zipfile
 from config import ROOT_DIR, SRC_DIR, OUT_DIR
+from typing import final
 
 from enums.DirectionEnum import DirectionEnum
 
@@ -24,6 +26,8 @@ class DatapackGeneratorUtils():
         self.z = z
         self.direction = direction
 
+        self.MAX_COMMANDS_PER_FILE: final = 1000
+
         # check if it's either x or z
         self.x_axis: bool = True
         if self.direction == DirectionEnum.NORTH.value or self.direction == DirectionEnum.SOUTH.value:
@@ -38,8 +42,11 @@ class DatapackGeneratorUtils():
             self.mult = -1
 
     def generate_datapack(self):
-        buffer_blocks = self.generate_place_initial_blocks()
-        buffer_item_frames = self.summon_item_frames()
+        PLACE_BLOCKS_NAME: final = 'place_initial_blocks'
+        SUMMON_ITEM_FRAMES_NAME: final = 'summon_item_frames'
+
+        buffer_dict_place_blocks = self.generate_place_initial_blocks()
+        buffer_dict_item_frames = self.summon_item_frames()
 
         path_datapack = os.path.join(OUT_DIR, f'datapack_{self.name}.zip')
         with zipfile.ZipFile(path_datapack, 'w', zipfile.ZIP_DEFLATED) as zipf:
@@ -49,14 +56,15 @@ class DatapackGeneratorUtils():
 
             # Write mfcuntions
             path_mcfunction = os.path.join('data', self.name.lower(), 'function')
-            path_initial_blocks = os.path.join(path_mcfunction, 'place_initial_blocks.mcfunction')
-            path_summon_item_frames = os.path.join(path_mcfunction, 'summon_item_frames.mcfunction')
             path_build = os.path.join(path_mcfunction, 'build.mcfunction')
             path_clean_item_frames = os.path.join(path_mcfunction, 'clean.mcfunction')
 
-            zipf.writestr(path_initial_blocks, buffer_blocks.getvalue())
-            zipf.writestr(path_summon_item_frames, buffer_item_frames.getvalue())
-            zipf.writestr(path_build, f'execute as @p run function {self.name}:place_initial_blocks\nexecute as @p run function {self.name}:summon_item_frames')
+            # place initial blocks & summon item frames
+            self.save_command_batches(zipf, path_mcfunction, buffer_dict_place_blocks, PLACE_BLOCKS_NAME)
+            self.save_command_batches(zipf, path_mcfunction, buffer_dict_item_frames, SUMMON_ITEM_FRAMES_NAME)
+
+            # doing both placing blocks and later summoning item frames
+            zipf.writestr(path_build, f'execute as @p run function {self.name}:{PLACE_BLOCKS_NAME}\nexecute as @p run function {self.name}:{SUMMON_ITEM_FRAMES_NAME}')
             
             # cleaning str
             cleaning_str = ''
@@ -91,7 +99,7 @@ class DatapackGeneratorUtils():
             zipf.writestr(path_clean_item_frames, cleaning_str)
 
 
-    def generate_place_initial_blocks(self):
+    def generate_place_initial_blocks(self) -> dict[str, io.BytesIO]:
         commands : list[str] = []
 
         # set commands required for each frame
@@ -145,10 +153,10 @@ class DatapackGeneratorUtils():
             commands.append(self.get_command_set_block('quartz_block',  self.x - 5, self.y + 1, '{0}'))
             # bottom
             commands.append(self.get_command_set_block('quartz_block',                        self.x,     self.y, '{0}'))
-            commands.append(self.get_command_set_block('quartz_stairs[facing=west,half=top]', self.x - 1, self.y, '{0}'))
+            commands.append(self.get_command_set_block('quartz_stairs[facing=east,half=top]', self.x - 1, self.y, '{0}'))
             commands.append(self.get_command_set_block('air',                                 self.x - 2, self.y, '{0}'))
             commands.append(self.get_command_set_block('air',                                 self.x - 3, self.y, '{0}'))
-            commands.append(self.get_command_set_block('quartz_stairs[facing=east,half=top]', self.x - 4, self.y, '{0}'))
+            commands.append(self.get_command_set_block('quartz_stairs[facing=west,half=top]', self.x - 4, self.y, '{0}'))
             commands.append(self.get_command_set_block('quartz_block',                        self.x - 5, self.y, '{0}'))
             # ice
             commands.append(self.get_command_set_block('packed_ice', self.x - 1, self.y - 1, '{0}'))
@@ -165,10 +173,10 @@ class DatapackGeneratorUtils():
             commands.append(self.get_command_set_block('quartz_block',  '{0}', self.y + 1, self.z - 5))
             # bottom
             commands.append(self.get_command_set_block('quartz_block',                          '{0}', self.y, self.z    ))
-            commands.append(self.get_command_set_block('quartz_stairs[facing=north,half=top]',  '{0}', self.y, self.z - 1))
+            commands.append(self.get_command_set_block('quartz_stairs[facing=south,half=top]',  '{0}', self.y, self.z - 1))
             commands.append(self.get_command_set_block('air',                                   '{0}', self.y, self.z - 2))
             commands.append(self.get_command_set_block('air',                                   '{0}', self.y, self.z - 3))
-            commands.append(self.get_command_set_block('quartz_stairs[facing=south,half=top]',  '{0}', self.y, self.z - 4))
+            commands.append(self.get_command_set_block('quartz_stairs[facing=north,half=top]',  '{0}', self.y, self.z - 4))
             commands.append(self.get_command_set_block('quartz_block',                          '{0}', self.y, self.z - 5))
             # ice
             commands.append(self.get_command_set_block('packed_ice', '{0}', self.y - 1, self.z - 1))
@@ -188,16 +196,9 @@ class DatapackGeneratorUtils():
                     parsed_command = command.format(self.z + index)
                 parsed_commands.append(parsed_command)
 
-        # Store in a buffer and return
-        buffer = io.BytesIO()
+        return self.generate_command_batches(parsed_commands, 'place_initial_blocks')
 
-        for command in parsed_commands:
-            buffer.write((command + '\n').encode('utf-8'))
-        buffer.seek(0)
-
-        return buffer
-
-    def summon_item_frames(self):
+    def summon_item_frames(self) -> dict[str, io.BytesIO]:
         # Item Frames have 6 possible places they face.
         # We're deciding the facing direction according the direction requested
         # Double bracket is used in order to escape literal curly braces in a format string avoiding python interpreter
@@ -241,25 +242,75 @@ class DatapackGeneratorUtils():
                 )
             parsed_commands.append(parsed_command)
 
-        # Store in a buffer and return
+        return self.generate_command_batches(parsed_commands, 'summon_item_frames')
+
+    def generate_command_batches(
+        self,
+        commands: list[str],
+        filename: str
+        ) -> dict[str, io.BytesIO]:
+        # Store commands in a buffer list
+        buffers: dict[str, io.BytesIO] = {}
+
+        counter = 1
+        first_iteration = 1
+        for index, cmd in enumerate(commands):
+            if index % self.MAX_COMMANDS_PER_FILE == 0:
+                if not first_iteration:
+                    buffer.seek(0)
+                    buffers[f'{filename}_{counter}'] = buffer
+                    counter += 1
+                buffer = io.BytesIO()
+                first_iteration = 0
+
+            buffer.write((cmd + '\n').encode('utf-8'))
+
+        if index % self.MAX_COMMANDS_PER_FILE != 0:
+            buffer.seek(0)
+            buffers[f'{filename}_{counter}'] = buffer
+        
+        return buffers
+
+    def save_command_batches(
+        self,
+        zipf: zipfile.ZipFile,
+        base_path: str,
+        buffer_dict: dict[str, io.BytesIO],
+        filename: str
+        ):
+        path_main = os.path.join(base_path, f'{filename}.mcfunction')
+        path_main_folder = os.path.join(base_path, filename)
         buffer = io.BytesIO()
-
-        for command in parsed_commands:
+        counter = 1
+        for key, value in buffer_dict.items():
+            # save ith buffer
+            curr_path = os.path.join(path_main_folder, f'{key}.mcfunction')
+            zipf.writestr(curr_path, value.getvalue())
+            # add to general place initial block call
+            command = f'schedule function {self.name}:{filename}/{key} {counter}t'
+            counter += 1
             buffer.write((command + '\n').encode('utf-8'))
+        # save general place initial block buffer into a file
         buffer.seek(0)
-
-        return buffer
+        zipf.writestr(path_main, buffer.getvalue())
+        pass
 
     def get_command_set_block(self, block_name, x, y, z):
         return f'setblock {x} {y} {z} {block_name}'
 
 if __name__ == '__main__':
     datapack_utils = DatapackGeneratorUtils(
-        total_frames=4,
-        x=-43,
-        y=122,
-        z=-60,
-        direction=DirectionEnum.WEST.value
+        name='odore_china',
+        total_frames=1000,
+        x=-10,
+        y=-60,
+        z=120,
+        direction=DirectionEnum.NORTH.value
     )
-
+    
     datapack_utils.generate_datapack()
+    
+    # d = datapack_utils.generate_place_initial_blocks()
+    # for key, value in d.items():
+    #     print(key, value.getvalue())
+
